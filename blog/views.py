e6acebeb908658edeb0fc5719e5d5625efdd6f49
AdminX <Vickky28091995@mail.ru>
2022-09-12 17:07:30 +0300

    2

---
+++ blog/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from . models import Blog
+def blog_index(request):
+    posts =Post.objects.all().order.by('-created_on')
+    context = {
+        'posts':posts
+
+    }
+    return render(request,'blog_index.html',context)
+
+
+def blog_detail(request,pk):
+    post = Post.objects.get(pk=pk)
+    context = {
+        'post': post
+
+    }
+    return render(request, 'blog_detail.html', context)
+
