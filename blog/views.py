dff1893ee470a93e9c36feb695e592d66870ea97
AdminX <Vickky28091995@mail.ru>
2022-09-16 21:27:04 +0300

    2

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
-from . models import Blog
+from . models import Post,Comment
+from . forms import CommentForm
+
 def blog_index(request):
-    posts =Post.objects.all().order.by('-created_on')
+    posts =Post.objects.all().order_by('-created_on')
     context = {
-        'posts':posts
+        'posts':posts,
 
     }
     return render(request,'blog_index.html',context)
@@ -12,9 +14,30 @@ def blog_index(request):
 
 def blog_detail(request,pk):
     post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method=='POST':
+        form =CommentForm(request.POST)
+        if form.is_valid():
+            comment=Comment(
+                author=form.cleaned_data['author'],
+                body = form.cleaned_data['body'],
+                post = post
+            )
+            comment.save()
+    comments= Comment.objects.filter(post=post)
     context = {
-        'post': post
+        'post': post,
+        'comments': comments,
+        'form': form
+ }
+    return render(request,'blog_detail.html',context)
 
+def blog_category(request,category):
+    posts=Post.objects.filter(
+        categories__name__contains=category
+    ).order_by('-created_on' )
+    context ={
+        'category': category,
+        'posts': posts
     }
-    return render(request, 'blog_detail.html', context)
-
+    return render(request,'blog_category.html',context)
